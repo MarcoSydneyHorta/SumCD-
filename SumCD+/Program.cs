5fc9bd97ed0b7f40ed593d58b59b123f7067dd23
Marco Antonio Sydney Horta <marco.sh@outlook.com>
2022-06-15 16:50:30 -0300

    Add project files.

---
+++ SumCD+/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumCD_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada = "";
+            string[] dados;
+            List<int> vs = new List<int>();
+            
+            Console.Write("Digite os dados separados por espaço: ");
+            entrada = Console.ReadLine();
+            dados = entrada.Split(" ");
+            int total = 0;
+            Solution.CalPoints(dados, ref vs);
+
+            int nList = vs.Count;
+            Console.Write("The total sum is = ");
+            for (int i = 0; i < vs.Count; i++)
+            {
+                if (i == vs.Count - 1)
+                {
+                    Console.Write(vs[i] + " = " + (vs[i] + total));
+                }
+                else
+                {
+                    Console.Write(vs[i] + " + ");
+                }
+                total += vs[i];
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ SumCD+/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SumCD_
+{
+    class Solution
+    {
+        public static  void CalPoints(string[] ops, ref List<int> vs)
+        {
+            int tam = ops.Length;
+
+            for (int i = 0; i < tam; i++)
+            {
+                switch (ops[i])
+                {
+                    case "C":   // Remove the last number
+                        vs.Remove(vs.Last());
+                        break;
+                    case "D":
+                        vs.Add(2 * vs.Last()); // Put 2*last number
+                        break;
+                    case "+":
+                        vs.Add(vs.Last() + vs.Skip(vs.Count() - 2).First());    // Put last number + penultimate number
+                        break;
+                    default:
+                        vs.Add(int.Parse(ops[i]));  // Put the number 
+                        break;
+                }
+            }
+
+        }
+    }
+}
+
